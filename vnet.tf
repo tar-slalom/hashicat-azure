df16334080db0e8140c6b2ebd26293800ad4742b
Troy Richards <troy.richards@slalom.com>
2021-05-13 18:55:51 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-module "azure network" {
+module "azure_network" {
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_space       = "10.0.0.0/16"
